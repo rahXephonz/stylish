69dc2f37771c700cca21a1e123f7ae030c9645c8
ioofy <mynev.id@gmail.com>
2022-09-17 23:17:19 +0700

    feat: import new components from Button for test

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Button, Tooltip } from "@stylish-ui/react";
+import { Tooltip, IconButton, Button } from "@stylish-ui/react";
 
 export default function App() {
   return (
     <div>
       Hello World This is
-      <Button />
       <Tooltip />
+      <Button />
+      <IconButton />
     </div>
   );
 }
