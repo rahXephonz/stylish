a9f321e45f9e9e2fe3e4efdcb76e8f9cc4a8515e
ioofy <mynev.id@gmail.com>
2022-09-19 21:07:23 +0800

    chore: remove declarations folder

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,8 +1,14 @@
 import { Tooltip, IconButton, Box, Button } from "@stylish-ui/react";
+import "./index.css";
 
 export default function App() {
   return (
-    <Box px={20}>
+    <Box
+      sx={{
+        p: "5px",
+        backgroundColor: "pink",
+      }}
+    >
       Hello World This is
       <Tooltip />
       <Button />
