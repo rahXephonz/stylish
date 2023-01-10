a3c9a7ee2a638daeab4150c0e9efe69da3d164e0
ioofy <mynev.id@gmail.com>
2023-01-10 18:32:16 +0800

    feat: adding stroke prop for every icons props

--- playgrounds/react/src/pages/home/index.tsx
+++ playgrounds/react/src/pages/home/index.tsx
@@ -1,39 +1,15 @@
-import { useEffect, useState } from "react";
 import { tw } from "twind";
-import { map } from "lodash-es";
 import Link from "next/link";
 import loadable from "@loadable/component";
 import { IconButton, Box, Stack, Wrap, WrapItem, Text, Badge } from "@stylish-ui/react";
 import { Belt, Cart } from "@stylish-ui/icons";
-import todosService from "../../services/todos";
 
 const LayoutLoadable = loadable(() => import("../../components/Layout"));
 
 const HomePage = () => {
-  const [todosArr, setTodosArr] = useState<Array<DataTodos>>([]);
-
-  useEffect(() => {
-    // this useeffect will running as async wrapped with async
-    (async function asyncFn() {
-      const res = await todosService.getAllTodos();
-      setTodosArr(res);
-    })();
-  }, []);
-
-  const arr = ["b", 1, 2, 3, "x"];
-  console.log(map("map Arr", arr));
-  console.log("client side fetching", todosArr);
-
   return (
-    <LayoutLoadable title="Home | Next.js + TypeScript Example">
+    <LayoutLoadable title="Home | Next.js Perfomance">
       <h1 className={tw("text-2xl text-green-500")}>Hello Next.js 👋</h1>
-      {todosArr
-        ? todosArr.map(item => (
-            <div key={item.id}>
-              <p>{item.title}</p>
-            </div>
-          ))
-        : null}
       <p>
         <Link href="/about">
           <a>About</a>
@@ -55,7 +31,7 @@ const HomePage = () => {
             <IconButton />
           </Box>
           <Box>
-            <Belt />
+            <Belt size={18} />
             <IconButton />
           </Box>
         </Stack>
